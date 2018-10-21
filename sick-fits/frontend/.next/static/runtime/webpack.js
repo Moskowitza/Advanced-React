db5ae56841e54c2105b3327c193695ae29484d35
moskowitza <moskowitzaaron@mac.com>
2018-10-21 08:20:30 -0400

    working on style

--- sick-fits/frontend/.next/static/runtime/webpack.js
+++ sick-fits/frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6378561476f960ab886a";
+/******/ 	var hotCurrentHash = "7e725eed9283b325b715";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
